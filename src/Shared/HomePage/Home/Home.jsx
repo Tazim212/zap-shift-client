a58fc8280b36cc4eaee5a9b9e28532b34059289c
Tazim212 <iftekhartazim21@gmail.com>
2026-06-26 21:30:19 +0600

    how it works section done

--- src/Shared/HomePage/Home/Home.jsx
+++ src/Shared/HomePage/Home/Home.jsx
@@ -1,9 +1,12 @@
+rsc
 import Banner from "../Banner/Banner";
+import WorksToDo from "../WorksToDo/WorksToDo";
 
 const Home = () =>{
     return(
         <div>
             <Banner></Banner>
+            <WorksToDo></WorksToDo>
         </div>
     )
 }
---
+++ src/Shared/HomePage/WorksToDo/WorksToDo.jsx
@@ -0,0 +1,63 @@
+import img from "../../../assets/bookingIcon.png"
+
+const WorksToDo = () => {
+    return (
+        <div className="mb-12">
+            <h1 className="text-3xl py-5 font-bold text-secondary">How It Works</h1>
+
+            <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-15">
+
+                <div className="card bg-gray-300 w-61 shadow-lg rounded-xl">
+                    <figure className="px-10 pt-10">
+                        <img
+                            src={img}
+                            alt="Shoes"
+                            className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Booking Pick & Drop</h2>
+                        <p>From personal packages to business shipments — we deliver on time, every time.</p>
+                    </div>
+                </div>
+                <div className="card bg-gray-300 w-61 shadow-lg rounded-xl">
+                    <figure className="px-10 pt-10">
+                        <img
+                            src={img}
+                            alt="Shoes"
+                            className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Cash On Delivery</h2>
+                        <p>From personal packages to business shipments — we deliver on time, every time.</p>
+                    </div>
+                </div>
+                <div className="card bg-gray-300 w-61 shadow-lg rounded-xl">
+                    <figure className="px-10 pt-10">
+                        <img
+                            src={img}
+                            alt="Shoes"
+                            className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Delivery Hub</h2>
+                        <p>From personal packages to business shipments — we deliver on time, every time.</p>
+                    </div>
+                </div>
+                <div className="card bg-gray-300 w-61 shadow-lg rounded-xl">
+                    <figure className="px-10 pt-10">
+                        <img
+                            src={img}
+                            alt="Shoes"
+                            className="rounded-xl" />
+                    </figure>
+                    <div className="card-body items-center text-center">
+                        <h2 className="card-title">Booking SME & Corporate</h2>
+                        <p>From personal packages to business shipments — we deliver on time, every time.</p>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+export default WorksToDo;
