0056084e6a0fc04df8061064c22b53a6e48e4c9f
Tazim212 <iftekhartazim21@gmail.com>
2026-07-30 21:47:42 +0600

    rider status update and delete part done

--- src/Pages/DashboardPages/ApproveRider.jsx
+++ src/Pages/DashboardPages/ApproveRider.jsx
@@ -3,6 +3,7 @@ import useAxiosSecure from "../../hooks/useAxiosSecure";
 import useAuth from "../../hooks/useAuth";
 import { FaTrashAlt, FaUserCheck } from "react-icons/fa";
 import { IoPersonRemove } from "react-icons/io5";
+import Swal from "sweetalert2";
 
 const ApproveRider = () => {
 
@@ -10,13 +11,59 @@ const ApproveRider = () => {
 
     const { user } = useAuth()
 
-    const { data: riders = [] } = useQuery({
+    const { data: riders = [], refetch } = useQuery({
         queryKey: ["riders"],
         queryFn: async () => {
             const res = await axiosSecure.get("/riders")
             return res.data;
         }
     })
+
+    const updateRiderInfo = (id, status) => {
+        axiosSecure.patch(`/riders/${id}?status=${status}`)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    refetch()
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `Your application has been ${status}`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
+
+    const handleAccept = (id) => {
+        updateRiderInfo(id, "Accepted")
+    }
+    const handleReject = (id) => {
+        updateRiderInfo(id, "Rejected")
+    }
+
+    const handleRiderDelet = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You want to delete the rider?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed)
+                axiosSecure.delete(`/riders/${id}`)
+                    .then(res => {
+                        refetch()
+                        Swal.fire({
+                            title: "Deleted!",
+                            text: "THe rider application has been deleted.",
+                            icon: "success"
+                        });
+                    })
+        });
+    }
 
     return (
         <div>
@@ -43,11 +90,11 @@ const ApproveRider = () => {
                                     <td>{rider.name}</td>
                                     <td>{rider.email}</td>
                                     <td>{rider.district}</td>
-                                    <td>{rider.status}</td>
+                                    <td className={`${rider.status === "Accepted" ? "text-green-500" : "text-red-600"}`}>{rider.status}</td>
                                     <td>
-                                        <button className="btn"><FaUserCheck /></button>
-                                        <button className="btn"><IoPersonRemove /></button>
-                                        <button className="btn"><FaTrashAlt /></button>
+                                        <button onClick={() => handleAccept(rider._id)} className="btn"><FaUserCheck /></button>
+                                        <button onClick={() => handleReject(rider._id)} className="btn"><IoPersonRemove /></button>
+                                        <button onClick={() => handleRiderDelet(rider._id)} className="btn btn-error"><FaTrashAlt /></button>
                                     </td>
                                 </tr>
                             )
